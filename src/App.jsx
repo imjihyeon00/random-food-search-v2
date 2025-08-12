c2c49ffe319943e37fa2eaab89db5528d067a2c1
imjihyeon00 <imjihyeon00@gmail.com>
2025-08-12 17:17:58 +0900

    Router 추가로 인해 페이지 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+// App.jsx
+import React, { Suspense } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
-function App() {
+function Layout() {
   return (
-    <ThemeProvider theme={{}}>
-      <GlobalStyle />
+    <>
       <Header />
-    </ThemeProvider>
+      <main style={{ minHeight: "60vh" }}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <ThemeProvider theme={{}}>
+      <GlobalStyle />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/contact" element={<Contact />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
+  );
+}
