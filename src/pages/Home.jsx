d145408c5eaa7bee6b65df0fb915654764847ec2
imjihyeon00 <imjihyeon00@gmail.com>
2025-08-15 12:15:34 +0900

    지도 추가 및 현재 위치에 마커 표시

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import Button from '../components/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { BUTTON_SIZES_TYPE } from '../constants/styled';
 import TryImage from '../assets/try_site.svg'; 
+import { Map, MapMarker } from "react-kakao-maps-sdk"; // 카카오 맵 컴포넌트
+
 export default function Home() {
+  const [mpaState, setMapState] = useState({
+    center: {
+      lat: 33.450701,
+      lng: 126.570667,
+    },
+    errMsg: null,
+    isLoading: true,
+  })
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setMapState((prev) => ({
+            ...prev,
+            center: {
+              lat: position.coords.latitude, // 위도
+              lng: position.coords.longitude, // 경도
+            },
+            isLoading: false,
+          }))
+        },
+        (err) => {
+          setMapState((prev) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false,
+          }))
+        }
+      )
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      setMapState((prev) => ({
+        ...prev,
+        errMsg: "geolocation을 사용할수 없어요..",
+        isLoading: false,
+      }))
+    }
+  }, [])
+
   return (
     <HomeContainer>
       <MapArea>
         <MapBox>
-          map위치
+          <Map // 지도를 표시할 Container
+            center={mpaState.center}
+            style={{
+              // 지도의 크기
+              width: "100%",
+              height: "100%",
+            }}
+            level={3} // 지도의 확대 레벨
+          >
+            {!mpaState.isLoading && (
+              <MapMarker position={mpaState.center}>
+                <div style={{ padding: "5px", color: "#000" }}>
+                  {mpaState.errMsg ? mpaState.errMsg : "여기에 계신가요?!"}
+                </div>
+              </MapMarker>
+            )}
+          </Map>
         </MapBox>
 
         <FilterArea>
