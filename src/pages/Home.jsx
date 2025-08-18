faf6888c5b64605c1df2aad4acc54369b46dd295
imjihyeon00 <imjihyeon00@gmail.com>
2025-08-18 14:08:49 +0900

    훅을 이용한 맵 노출 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import Button from '../components/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,66 +5,31 @@ import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { BUTTON_SIZES_TYPE } from '../constants/styled';
 import TryImage from '../assets/try_site.svg'; 
 import { Map, MapMarker } from "react-kakao-maps-sdk"; // 카카오 맵 컴포넌트
+import useMapController from '../hook/useMapController';
+import { FILTER_LIST } from '../constants/filter';
 
 export default function Home() {
-  const [mpaState, setMapState] = useState({
-    center: {
-      lat: 33.450701,
-      lng: 126.570667,
-    },
-    errMsg: null,
-    isLoading: true,
-  })
+  const {
+  center, isLoading, errMsg, onMapClick,
+  chip, setChip,
+  searchFood,
+} = useMapController({ initialChip: FILTER_LIST[0], radius: 3000 });
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setMapState((prev) => ({
-            ...prev,
-            center: {
-              lat: position.coords.latitude, // 위도
-              lng: position.coords.longitude, // 경도
-            },
-            isLoading: false,
-          }))
-        },
-        (err) => {
-          setMapState((prev) => ({
-            ...prev,
-            errMsg: err.message,
-            isLoading: false,
-          }))
-        }
-      )
-    } else {
-      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-      setMapState((prev) => ({
-        ...prev,
-        errMsg: "geolocation을 사용할수 없어요..",
-        isLoading: false,
-      }))
-    }
-  }, [])
 
   return (
     <HomeContainer>
       <MapArea>
         <MapBox>
-          <Map // 지도를 표시할 Container
-            center={mpaState.center}
-            style={{
-              // 지도의 크기
-              width: "100%",
-              height: "100%",
-            }}
-            level={3} // 지도의 확대 레벨
+          <Map
+            center={center}
+            style={{ width: "100%", height: "100%" }}
+            level={3}
+            onClick={onMapClick}
           >
-            {!mpaState.isLoading && (
-              <MapMarker position={mpaState.center}>
-                <div style={{ padding: "5px", color: "#000" }}>
-                  {mpaState.errMsg ? mpaState.errMsg : "여기에 계신가요?!"}
+            {!isLoading && (
+              <MapMarker position={center}>
+                <div style={{ padding: 5, color: "#000" }}>
+                  {errMsg ? errMsg : "여기에 계신가요?!"}
                 </div>
               </MapMarker>
             )}
@@ -79,41 +43,19 @@ export default function Home() {
               <span> 필터링</span>
             </p>
             <div className='filter-list'>
-              <Button 
-                text="전체" 
-                className="active" 
-                onClick={() => {}}
-              />
-              <Button 
-                text="한식" 
-                onClick={()=>{}}
-              />
-              <Button 
-                text="양식" 
-                onClick={()=>{}}
-              />
-              <Button 
-                text="아시아음식" 
-                onClick={()=>{}}
-              />
-              <Button 
-                text="일식" 
-                onClick={()=>{}}
-              />
-              <Button 
-                text="중식" 
-                onClick={()=>{}}
-              />
-              <Button 
-                text="분식" 
-                onClick={()=>{}}
-              />
-              {/* <Button text="카페/디저트" /> */}
+              {FILTER_LIST.map((c) => (
+                <Button
+                  key={c}
+                  onClick={() => setChip(c)}
+                  active={chip==c}
+                  text={c}
+                />
+              ))}
             </div>
           </FilterBox>
           <Button 
             text="랜덤 음식점 찾기" 
-            onClick={() => alert('랜덤 음식점 찾기 클릭!')} 
+            onClick={()=>{searchFood()}} 
             fullWidth={true}
             size={BUTTON_SIZES_TYPE.lg}
           />
