53872cf77a00008229ba00808fc9a3cdf271abe3
imjihyeon00 <imjihyeon00@gmail.com>
2025-08-27 01:23:56 +0900

    404 페이지 추가

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,6 @@
+import ImageMessage from "../components/ImageMessage";
+import Error404Img from "../assets/404_error_icon.svg"
+
 export default function NotFound() {
-  return <div>404 - Not Found</div>;
+  return <ImageMessage src={Error404Img} text="찾을 수 없는 페이지 입니다."></ImageMessage>;
 };
