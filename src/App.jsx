a3ea64b0e52511baf6ad03acc99b055f25b7276a
imjihyeon00 <imjihyeon00@gmail.com>
2025-08-31 19:41:24 +0900

    이용약관, 개인정보 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
+import TermsOfServicePage from "./pages/TermsOfServicePage";
 import NotFound from "./pages/NotFound";
 import { useKakaoLoader } from "react-kakao-maps-sdk";
+import useScrollToTop from "./hook/useScrollToTop";
 
 function Layout() {
   return (
@@ -29,6 +32,9 @@ export default function App() {
     libraries: ["services"],                    // ← Places 필수
   });
 
+  // 페이지 전환 시 최상단 스크롤
+  useScrollToTop();
+
   if (error) return <div>카카오 지도 로드 오류: {String(error)}</div>;
   if (loading) return <div>지도를 준비 중…</div>;
   return (
@@ -37,8 +43,9 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms" element={<TermsOfServicePage />} />
+          {/* 
           <Route path="/contact" element={<Contact />} /> */}
           <Route path="*" element={<NotFound />} />
         </Route>
